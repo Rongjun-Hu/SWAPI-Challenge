385ef08528dfa4a4341092ec28ea391ef3051ee9
Peter <rongjun.hu.0716@gmail.com>
2021-06-23 14:40:01 +1000

    Film List Component display a list of films.

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const FilmList = ({ films }) => {
+const FilmList = ({ films, favouriteClick, favoriteComponent }) => {
+  const FavouriteComponent = favoriteComponent;
+
   return (
     <>
-      {films.map((film) => (
-        <div key={film.episode_id} className="flex-row">
-          <Link to={`/films/` + film.episode_id}>
-            <div className="flex-col">
-              <div className="film container">
-                <div className="film-heading">
-                  <h1>{film.title}</h1>
-                  <p>{film.release_date}</p>
-                </div>
-                <div className="film-intro">
-                  <p>{film.opening_crawl}</p>
+      {films.map((film, filmIndex) => (
+        <div key={filmIndex} className="flex-row">
+          <div className="flex-col">
+            <div className="film container">
+              <div className="film-heading">
+                <h1>{film.title}</h1>
+                <p>{film.release_date}</p>
+
+                <div className="film-buttons">
+                  <button className="film-buttons-details">
+                    <Link to={`/films/` + (filmIndex + 1)}>Details</Link>
+                  </button>
+
+                  <button
+                    className="film-buttons-favourite"
+                    onClick={() => favouriteClick(film)}
+                  >
+                    Favourite
+                  </button>
                 </div>
               </div>
+              <div className="film-intro">
+                <p>{film.opening_crawl}</p>
+              </div>
             </div>
-          </Link>
+          </div>
         </div>
       ))}
     </>
